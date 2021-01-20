78813b118df32e9f869594d90e2bcd42c9d992b6
Wei Lee <weilee.rx@gmail.com>
2021-01-20 11:42:58 +0800

    test(collector): add test cases to PelicanArticleDataCollector

---
+++ tests/sample_pelican_project/content/article_1.md
@@ -0,0 +1,8 @@
+Title: this is title
+Date: 2021-01-19 16:12
+Category: Testing
+Tags: test_tag
+Slug: this_is_title
+Authors: Lee-W
+
+content
---
+++ tests/test_collector.py
@@ -0,0 +1,38 @@
+import pelican
+from pelican_stat import collector
+
+
+def test_get_pelican_instance():
+    pelican_instance = collector.PelicanArticleDataCollector._get_pelican_instance(
+        "tests/sample_pelican_project/pelicanconf.py"
+    )
+    assert isinstance(pelican_instance, pelican.Pelican)
+
+
+def test_collect_articles():
+    data_collector = collector.PelicanArticleDataCollector(
+        "tests/sample_pelican_project/pelicanconf.py"
+    )
+    data_collector.collect_articles()
+
+    assert len(data_collector.articles) == 1
+
+    article = data_collector.articles[0]
+    assert article.title == "this is title"
+    assert article.category.name == "Testing"
+    assert article.slug == "this_is_title"
+
+
+def test_export(tmp_path, data_regression):
+    output_path = tmp_path / "result.json"
+
+    data_collector = collector.PelicanArticleDataCollector(
+        "tests/sample_pelican_project/pelicanconf.py"
+    )
+    data_collector.collect_articles()
+    data_collector.export(output_path)
+
+    with open(output_path, "r") as output_file:
+        output_data = output_file.read()
+
+    data_regression.check(output_data)
