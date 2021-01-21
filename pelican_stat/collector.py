8dbfb023b879a70176794c0dfc2cbffd3a2e516c
Wei Lee <weilee.rx@gmail.com>
2021-01-21 16:53:08 +0800

    refactor(collector): add extract_articles_info

--- pelican_stat/collector.py
+++ pelican_stat/collector.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from typing import List, Union
+from typing import List, Union, Dict
 
 import pelican
 
@@ -9,6 +9,7 @@ class PelicanArticleDataCollector:
     def __init__(self, pelican_conf_path: str):
         self.pelican_instance = self._get_pelican_instance(pelican_conf_path)
         self.articles: List[pelican.contents.Article] = []
+        self.collect_articles()
 
     @staticmethod
     def _get_pelican_instance(conf_path: str) -> pelican.Pelican:
@@ -43,19 +44,23 @@ class PelicanArticleDataCollector:
         article_generator.generate_context()
         self.articles = article_generator.articles
 
-    def export(self, output_path: Union[Path, str]) -> None:
-        article_info = [
+    def extract_articles_info(self) -> List[Dict[str, Union[str, int]]]:
+        articles_info: List[Dict[str, Union[str, int]]] = [
             {
                 "timestamp": article.date.timestamp(),
                 "category": article.category.name,
-                "authors": [author.name for author in article.authors],
+                "authors": [author.name for author in article.authors],  # type: ignore
                 "reader": article.reader,
                 "status": article.status,
-                "tags": [tag.name for tag in article.tags],
+                "tags": [tag.name for tag in article.tags],  # type: ignore
                 "timezone": str(article.timezone),
                 "title": article.title,
             }
             for article in self.articles
         ]
+        return articles_info
+
+    def export(self, output_path: Union[Path, str]) -> None:
+        articles_info = self.extract_articles_info()
         with open(output_path, "w") as f:
-            json.dump(article_info, f, ensure_ascii=False, indent=4)
+            json.dump(articles_info, f, ensure_ascii=False, indent=4)
